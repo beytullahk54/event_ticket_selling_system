2ec39fe1d7bab6a2b13ed204d9c3e94b70bb98ae
beytullah <kahramanbey54@gmail.com>
2025-08-01 00:06:44 +0300

    Model güncellemeleri

--- backend/app/Models/Event.php
+++ backend/app/Models/Event.php
@@ -9,7 +9,7 @@ class Event extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'image_url', 'event_date', 'venue_id'];
+    protected $fillable = ['title', 'description', 'image_url', 'event_date', 'venue_id', 'amount'];
 
     public function venue()
     {
--- backend/app/Models/Payment.php
+++ backend/app/Models/Payment.php
@@ -9,5 +9,5 @@ class Payment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['order_id', 'total_amount', 'cc_number', 'cc_exp_month', 'cc_exp_year', 'cc_cvv'];
+    protected $fillable = ['order_id', 'total_amount', 'cc_number', 'cc_exp_month', 'cc_exp_year', 'cc_exp_cvv'];
 }
