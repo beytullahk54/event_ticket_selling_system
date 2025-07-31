3688003f021bc9676533fa72c2af914bb22e616f
beytullah <kahramanbey54@gmail.com>
2025-08-01 00:08:15 +0300

    route name düzenlemesi yaptık

---
+++ frontend/app/pages/seat-plans/[event_id]/[category_id].vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="mb-8 p-4">
+        <EventsBus :eventId="eventId" />
+    </div>
+
+    <div class=" flex justify-end " >
+          <NuxtLink to="/payment" class="btn btn-secondary btn-lg m-4 gap-2 w-1/4">
+            Bilet Oluştur
+          </NuxtLink>
+      </div>
+</template>
+
+<script setup>
+  const route = useRoute()
+  const eventId = route.params.event_id
+  const categoryId = route.params.category_id
+
+  const ticketStore = useMyTicketStore()
+
+  onMounted(async () => {
+      ticketStore.setEventId(eventId)
+      ticketStore.setCategoryId(categoryId)
+  })
+</script>
