b86d90d9fc3ba864b69eec8c7eaf85ee6326dc01
beytullah <kahramanbey54@gmail.com>
2025-07-31 21:36:54 +0300

    Front listeleme sayfasının revize edilmesi

--- frontend/app/components/events/list.vue
+++ frontend/app/components/events/list.vue
@@ -1,19 +1,20 @@
 <template>
-    <div class="container mx-auto py-8" >
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div v-for="event in events" :key="event.id" class="card bg-base-100 shadow-xl">
-            <NuxtLink :to="`/event/${event.id}`">
-              <figure>
-                <img :src="event.image" alt="Event Image" class="w-full h-48 object-cover" />
-              </figure>
-                <div class="card-body">
-                <h2 class="card-title">{{ event.name }}</h2>
-                <p>{{ event.description }}</p>
-                </div>
-            </NuxtLink>
-          </div>
-        </div>
+  <div class="container mx-auto py-8" >
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div v-for="event in events" :key="event.id" class="card bg-base-100 shadow-xl">
+        <NuxtLink :to="`/events/${event.id}`">
+          <figure>
+            <img :src="event?.image_url" alt="Event Image" class="w-full h-48 object-cover" />
+          </figure>
+            <div class="card-body">
+            <h2 class="card-title">{{ event?.title.length > 35 ? event?.title.substring(0, 35) + '...' : event?.title }}</h2>
+            <!-- <p>{{ event?.description }}</p> -->
+             {{ event?.venue?.name }} / {{ dateUtils.formatDate(event?.event_date) }}
+            </div>
+        </NuxtLink>
       </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
