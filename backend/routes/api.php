3afac2ed152d9414ca5317dd7d24aa366e0d2762
beytullah <kahramanbey54@gmail.com>
2025-07-31 21:20:52 +0300

    Ticket'sın kaldırılıp payments üzerinden işlem yapılacak şekilde düzenlenmesi

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,12 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\TicketController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::resource('events', EventController::class);
-Route::resource('tickets', TicketController::class);
 Route::resource('payments', PaymentController::class); 
